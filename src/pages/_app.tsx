ba03c6e737d256f6089a5c0f459ad99b19854984
MekkaLabs <153198782+cannabizbrasil@users.noreply.github.com>
2024-04-29 17:05:46 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import { HubspotProvider } from 'next-hubspot';
+import type { AppProps } from 'next/app'
+import { GoogleTagManager } from '@next/third-parties/google'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${poppins.className}`}>
+      <HubspotProvider>
+        <Component {...pageProps} />
+        <GoogleTagManager gtmId="GTM-M43DX64N" />
+      </HubspotProvider>
+    </main>
+
+
+  )
 }
